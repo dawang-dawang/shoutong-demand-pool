dafae66c558e964525a756083e3009eebd70d129
dawang-dawang <1149504465@qq.com>
2026-09-24 17:29:48 +0800

    deploy: config.js @ 2026-09-24T09:29:49.135Z

---
+++ config.js
@@ -0,0 +1,5 @@
+// 本文件由 src/tools/inject_config.js 自动生成，请勿手改
+window.__APP_CONFIG__ = {
+  SUPABASE_URL: "https://ioetqcowpsxwjwqayxlp.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZXRxY293cHN4d2p3cWF5eGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMjY4NzQsImV4cCI6MjEwNTgwMjg3NH0.zdMEE4hXfgn713k369uG463vhmeRtF6hQmjoeFW9qWU"
+};
